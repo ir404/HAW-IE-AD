2cc9425cacd26f4d666c590883d29476089df035
Imran <67403229+ir404@users.noreply.github.com>
2025-12-12 15:48:55 +0100

    Update TreeTest with new student entries and checks

--- LabWork/src/tree/TreeTest.java
+++ LabWork/src/tree/TreeTest.java
@@ -2,14 +2,12 @@ package tree;
 
 import data.Student;
 
-import java.util.Iterator;
-
 public class TreeTest {
     public void start() {
         SortedBinaryTree<Node> classA = new SortedBinaryTree<>();
         classA.insert(new StudentItem(new Student("Alice", 10)));
-        classA.insert(new StudentItem(new Student("Bob", 5)));
-        classA.insert(new StudentItem(new Student("Charlie", 15)));
+        classA.insert(new StudentItem(new Student("Bob", 1)));
+        classA.insert(new StudentItem(new Student("Charlie", 5)));
         classA.insert(new StudentItem(new Student("David", 2)));
         classA.insert(new StudentItem(new Student("Eve", 12)));
 
@@ -17,8 +15,11 @@ public class TreeTest {
         classB.insert(new StudentItem(new Student("Frank", 8)));
         classB.insert(new StudentItem(new Student("Grace", 3)));
         classB.insert(new StudentItem(new Student("Heidi", 14)));
-        classB.insert(new StudentItem(new Student("Ivan", 1)));
+        classB.insert(new StudentItem(new Student("Ivan", 5)));
         classB.insert(new StudentItem(new Student("Judy", 9)));
+        classB.insert(new StudentItem(new Student("James", 7)));
+        classB.insert(new StudentItem(new Student("Gwen", 6)));
+        classB.insert(new StudentItem(new Student("Myo", 4)));
 
         System.out.println("\n[Class A before merge]:");
         printTree(classA);
@@ -26,19 +27,43 @@ public class TreeTest {
         System.out.println("\n[Class B before merge]:");
         printTree(classB);
 
-        // Merge Class B into Class A
-        System.out.println("\n... Merging Class B into Class A ...");
-        classA.merge(classB);
 
-        // Verify the result (Sorted Master List)
-        System.out.println("\n[Master List (Sorted)]:");
-        printTree(classA);
+        System.out.println("\n[Class B balanced?]: " + classB.isBalanced());
+        
+        
+        
+
+
+        System.out.println("\n[Class B height]: " + classB.getHeight());
+
+//        // Merge Class B into Class A
+//        System.out.println("\n... Merging Class B into Class A ...");
+//        classB.merge(classA);
+//
+//        // Verify the result (Sorted Master List)
+//        System.out.println("\n[Master List (Sorted)]:");
+//        printTree(classB);
+//        
+//        int searchKey = 16;
+//        Node result = classB.find(searchKey);
+//        System.out.println("\n\nFind " + searchKey + " in Class B");
+//        if (result != null) {
+//        	System.out.println("Student name: " + result.toString());
+//        }
+//        else {
+//        	System.out.println("No student with grade " + searchKey);
+//    	}
     }
 
     public void printTree(SortedBinaryTree<Node> tree) {
-        Iterator<Node> it = tree.iterator();
-        while (it.hasNext()) {
-            System.out.println(it.next());
+//        Iterator<Node> it = tree.iterator();
+//        while (it.hasNext()) {
+//            System.out.println(it.next());
+//        }
+//        System.out.println();
+        for(Node n: tree) {
+        	System.out.println(n);
         }
     }
+    
 }
